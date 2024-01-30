7bf66e810308c7d29982b35fef8818ddaef1118d
superorange <superorange@tom.com>
2024-01-30 13:00:07 +0000

    更改存储结构

--- api/models.py
+++ api/models.py
@@ -5,12 +5,8 @@ import os
 # Create your models here.
 class InfoGroup(models.Model):
     id = models.BigAutoField(primary_key=True)
-    sub_id = models.CharField(max_length=32)
-    site = models.CharField(max_length=200)
-    user_name = models.CharField(max_length=200)
-    password = models.CharField(max_length=200)
-    iv = models.BinaryField(max_length=16, default=partial(os.urandom, 16))
-    tags = models.JSONField()
+    data = models.BinaryField(max_length=2048)
+    salt = models.BinaryField(max_length=32, default=partial(os.urandom, 32))
     update_time = models.DateField(auto_now=True)
     create_time = models.DateField(auto_now_add=True)
     owner = models.ForeignKey(to="login.User", on_delete=models.CASCADE)
