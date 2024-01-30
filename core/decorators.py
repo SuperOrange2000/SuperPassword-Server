89c8f8d132923162d92d5ba96648fafa07e6c2bd
superorange <superorange@tom.com>
2024-01-30 13:26:47 +0000

    增加错误信息打印

--- core/decorators.py
+++ core/decorators.py
@@ -1,6 +1,7 @@
 from functools import wraps
 from django.http import HttpRequest, JsonResponse
 from typing import List, Literal
+import traceback
 
 def require_http_methods(methods:List[Literal["GET", "POST", "PUT", "DELETE"]]):
     def decorator(func):
@@ -21,12 +22,13 @@ def handle_exceptions(func):
         try:
             return func(request)
         except KeyError as e:
+            print(f"缺少参数{e}")
             return JsonResponse(
                 status=422,
                 data={"message":f"缺少参数{e}"}
             )
-        except Exception as e:
-            print(f"Exception {e} occurred in {func.__name__}")
+        except Exception:
+            traceback.print_exc()
             return JsonResponse(
                 status=500,
                 data={"message":"服务器未知错误"}
