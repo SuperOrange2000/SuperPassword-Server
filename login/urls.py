925af8154a8f767a69696639e11bfc85f6b41fe1
superorange <superorange@tom.com>
2024-03-31 06:20:01 +0000

    修改路径

--- login/urls.py
+++ login/urls.py
@@ -3,7 +3,7 @@ from django.urls import path
 from . import main
 
 urlpatterns = [
-    path("csrf/", main.get_csrf_token, name="csrf"),
-    path("sign-up/", main.sign_up, name="sign up"),
-    path("login/", main.login, name="login"),
+    path("csrf", main.get_csrf_token, name="csrf"),
+    path("sign-up", main.sign_up, name="sign up"),
+    path("login", main.login, name="login"),
 ]
